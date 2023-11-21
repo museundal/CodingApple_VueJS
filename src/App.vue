6a4d8a7ffa2528afaffa16cbab1bded76f1a83bb
museundal <ehdus1105@gmail.com>
2023-11-22 00:40:06 +0900

    [:hammer:] 이벤트 헨들러 (1)

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,19 @@
   <div class="menu">
     <a v-for="메뉴 in 메뉴들" :key="메뉴">{{ 메뉴 }}</a>
   </div>
-  <div v-for="product in products" :key="product">
-    <h4>{{ product }}</h4>
-    <p> 50 만원</p>
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>50만원</p>
+    <button @click="increase()">허위매물신고</button>
+    <span>신고수 : {{ 신고수 }} </span>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>50만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>50만원</p>
   </div>
 </template>
 
@@ -15,8 +25,14 @@ export default {
   name: 'App',
   data() {
     return {
+      신고수: 0,
       products: ['마포구 원룸', '관악구 원룸', '송파구 원룸'],
-      메뉴들: ['Home', 'Shop', 'About']
+      메뉴들: ['Home', 'Shop', 'About'],
+    }
+  },
+  methods: {
+    increase() {
+      this.신고수 += 1;
     }
   },
   components: {
