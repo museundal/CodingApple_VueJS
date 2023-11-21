c835af78f5b06d7e74b70a4ed901d7fdd9708e8b
museundal <ehdus1105@gmail.com>
2023-11-22 00:48:59 +0900

    [:hammer:] 신고수 배열 숙제

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,20 @@
   <div>
     <h4>{{ products[0] }}</h4>
     <p>50만원</p>
-    <button @click="increase()">허위매물신고</button>
-    <span>신고수 : {{ 신고수 }} </span>
+    <button @click="increase(0)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[0] }} </span>
   </div>
   <div>
     <h4>{{ products[1] }}</h4>
     <p>50만원</p>
+    <button @click="increase(1)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[1] }} </span>
   </div>
   <div>
     <h4>{{ products[2] }}</h4>
     <p>50만원</p>
+    <button @click="increase(2)">허위매물신고</button>
+    <span>신고수 : {{ 신고수[2] }} </span>
   </div>
 </template>
 
@@ -25,14 +29,14 @@ export default {
   name: 'App',
   data() {
     return {
-      신고수: 0,
+      신고수: [0, 0, 0],
       products: ['마포구 원룸', '관악구 원룸', '송파구 원룸'],
       메뉴들: ['Home', 'Shop', 'About'],
     }
   },
   methods: {
-    increase() {
-      this.신고수 += 1;
+    increase(index) {
+      this.신고수[index] += 1;
     }
   },
   components: {
