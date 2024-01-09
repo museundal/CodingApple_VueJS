a2afed928b9f91b1a2720e91df44258a468bcbef
museundal <ehdus1105@gmail.com>
2024-01-09 16:10:33 +0900

    :hammer:props까지 배움

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
-  <div class="black-bg" v-if="모달창열렸니 == true">
-    <div class="white-bg">
-      <img :src="원룸들[누른거].image">
-      <h4>{{ 원룸들[누른거].title }}</h4>
-      <p>{{ 원룸들[누른거].content }}</p>
-      <p>{{ 원룸들[누른거].price }}</p>
-      <button @click="모달창열렸니 = false">닫기</button>
-    </div>
-  </div>
+  <Modal :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니" />
 
   <div class="menu">
     <a v-for="메뉴 in 메뉴들" :key="메뉴">{{ 메뉴 }}</a>
   </div>
+
+  <Discount />
+
   <div v-for="(oneroom, i) in 원룸들" :key="i">
     <img :src="oneroom.image" class="room-img">
     <h4 @click="모달창열렸니 = true; 누른거 = i">{{ oneroom.title }}</h4>
@@ -21,7 +16,8 @@
 
 <script>
 import data from './assets/oneroom.js';
-
+import Discount from './components/DiscountBanner.vue';
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'App',
@@ -36,7 +32,7 @@ export default {
     }
   },
   components: {
-
+    Discount, Modal,
   }
 }
 </script>
@@ -48,21 +44,6 @@ export default {
 
 div {
   box-sizing: border-box;
-}
-
-.black-bg {
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  position: fixed;
-  padding: 20px;
-}
-
-.white-bg {
-  width: 100%;
-  background-color: white;
-  border-radius: 8px;
-  padding: 20px;
 }
 
 .room-img {
---
+++ src/components/Modal.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="black-bg" v-if="모달창열렸니 == true">
+        <div class="white-bg">
+            <img :src="원룸들[누른거].image" style="width: 50%;">
+            <h4>{{ 원룸들[누른거].title }}</h4>
+            <p>{{ 원룸들[누른거].content }}</p>
+            <p>{{ 원룸들[누른거].price }}</p>
+            <Discount />
+            <!-- <button @click="모달창열렸니 = false">닫기</button> -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Modal',
+    props: {
+        원룸들: Array,
+        누른거: Number,
+        모달창열렸니: Boolean
+    }
+}
+</script>
+
+<style>
+.black-bg {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    padding: 20px;
+}
+
+.white-bg {
+    width: 100%;
+    background-color: white;
+    border-radius: 8px;
+    padding: 20px;
+}
+</style>
